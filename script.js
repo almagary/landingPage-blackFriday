b2087acb22243ef8492fa9deb259afe742697341
almagary <almagary2008@gmail.com>
2021-07-17 13:27:36 -0300

    added js

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+
+    let nome = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+   console.log(nome,email);
+
+   let data = {
+       nome,
+       email,
+   }
+
+   let convertData =JSON.stringify(data);
+
+   localStorage.setItem('lead', convertData);
+
+})
